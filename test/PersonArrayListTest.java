2d2c1e7df466848a840bb112b0dbb32210c55128
sungwoo96 <tjddn703@naver.com>
2022-04-04 18:22:14 +0900

    add StreamTest

--- test/PersonArrayListTest.java
+++ test/PersonArrayListTest.java
@@ -1,14 +1,19 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
 public class PersonArrayListTest {
-    ArrayList<Person> list = new ArrayList<>() {};
+    List<Person> list = new ArrayList<>() {};
 
     Person teacher = new Person("선생님",35,80.0,2);
     Person student = new Person("학생",17,60,2);
+
+    Stream<Person> listStream = list.stream();
 
     @Test
     public void 리스트출력() throws Exception{
@@ -17,7 +22,10 @@ public class PersonArrayListTest {
         list.add(teacher);
         list.add(student);
         //then
-        for (Person p:list) { System.out.println(p);}
+        /* for Each 문 */
+        //for (Person p:list) { System.out.println(p);}
+        list.forEach(System.out::println);
+
 
     } // 리스트출력()
 
@@ -35,6 +43,18 @@ public class PersonArrayListTest {
         /*객체 비교 */
         assertEquals(p1,teacher);
         assertEquals(p2,student);
+    }
+
+    @Test
+    public void 스트림테스트() throws Exception {
+        //given
+        List<String> list = Arrays.asList("student", "teacher", "police", "fireman" );
+        //when
+        /*"a" 포함하는 문자열*/
+        Stream<String> stream = list.stream().filter(name -> name.contains("a"));
+        //then
+        stream.forEach(ps -> System.out.println(ps+" "));
+
     }
 
 } // PersonArrayListTest
